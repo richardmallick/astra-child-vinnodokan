1c8fbb6039424b43fa4a68d1ac31e240d55254d5
richardmallick <richardsetu@gmail.com>
2024-02-21 02:13:42 +0600

    Added header styles

--- functions.php
+++ functions.php
@@ -2,11 +2,34 @@
 /**
  * enqueue styles and scripts
  */
-add_action('wp_enqueue_scripts', 'hooldsly_enqueue_styles');
-function hooldsly_enqueue_styles()
+add_action('wp_enqueue_scripts', 'vinnodokan_enqueue_styles');
+function vinnodokan_enqueue_styles()
 {
     $parent_style = 'parent-style';
     wp_enqueue_style($parent_style, get_template_directory_uri() . '/style.css');
     wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css', array(), time());
 
 }
+
+/**
+ * 
+ */
+add_action( 'astra_header_before', function(){
+    ?>
+    <div class="vinnodokan-top-bar-wrapper">
+        <div class="ast-container vinnodokan-top-bar-container">
+            <div class="vinnodokan-top-bar">
+                <div class="vinnodokan-top-bar-left">
+                    <p class="vinnodokan-top-lap"><?php echo esc_html( 'Please Call us to check the Stock before Placing the order!' ); ?></p>
+                    <p class="vinnodokan-top-mob"><?php echo esc_html( 'Please Call us to check the Stock!' ); ?></p>
+                </div>
+                <div class="vinnodokan-top-bar-right">
+                    <p><span>Call Us:</span> 01988232393</p>
+                    <a href="#">FB Page</a>
+                    <a href="#">FB Group</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php
+} );
